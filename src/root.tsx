4bcc93bfd6bdd49b066652cf46b259c15cff57d0
Anderson Sousa <and3rsonsousa@outlook.com>
2023-03-20 20:07:29 -0300

    pixel

--- src/root.tsx
+++ src/root.tsx
@@ -1,41 +1,46 @@
 import { component$ } from "@builder.io/qwik";
 import {
-	QwikCityProvider,
-	RouterOutlet,
-	ServiceWorkerRegister,
+  QwikCityProvider,
+  RouterOutlet,
+  ServiceWorkerRegister,
 } from "@builder.io/qwik-city";
 import { RouterHead } from "./components/router-head/router-head";
 
 import "./global.css";
 
 export default component$(() => {
-	return (
-		<QwikCityProvider>
-			<head>
-				<meta charSet="utf-8" />
-				<link rel="manifest" href="/manifest.json" />
-				<link rel="icon" href="/icon.png" />
+  return (
+    <QwikCityProvider>
+      <head>
+        <meta charSet="utf-8" />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="icon" href="/icon.png" />
 
-				{/* <script dangerouslySetInnerHTML="!function(f,b,e,v,n,t,s)
-        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-        n.queue=[];t=b.createElement(e);t.async=!0;
-        t.src=v;s=b.getElementsByTagName(e)[0];
-        s.parentNode.insertBefore(t,s)}(window, document,'script',
-        'https://connect.facebook.net/en_US/fbevents.js');
-        fbq('init', '160659696850852');
-        fbq('track', 'PageView');"/> */}
 
-				<RouterHead />
-			</head>
-			<body lang="pt-br">
-				{/* <noscript><img height="1" width="1" style="display:none"
-          src="https://www.facebook.com/tr?id=160659696850852&ev=PageView&noscript=1"
-        /></noscript> */}
-				<RouterOutlet />
-				<ServiceWorkerRegister />
-			</body>
-		</QwikCityProvider>
-	);
+
+        <script dangerouslySetInnerHTML="
+  !function(f,b,e,v,n,t,s)
+  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+  n.queue=[];t=b.createElement(e);t.async=!0;
+  t.src=v;s=b.getElementsByTagName(e)[0];
+  s.parentNode.insertBefore(t,s)}(window, document,'script',
+  'https://connect.facebook.net/en_US/fbevents.js');
+  fbq('init', '5942616745834540');
+  fbq('track', 'PageView');" />
+
+
+
+        <RouterHead />
+      </head>
+      <body lang="pt-br">
+        <noscript><img height="1" width="1" style="display:none"
+          src="https://www.facebook.com/tr?id=5942616745834540&ev=PageView&noscript=1"
+        /></noscript>
+        <RouterOutlet />
+        <ServiceWorkerRegister />
+      </body>
+    </QwikCityProvider>
+  );
 });
