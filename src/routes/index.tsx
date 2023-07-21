df8d82020dd1d3cf8b99505ae66060dfc84d2428
Anderson <and3rsonsousa@outlook.com>
2023-07-21 14:16:03 -0300

    especialidades

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -12,10 +12,87 @@ export default component$(() => {
 					Agende sua consulta
 				</a>
 
+				<div class="grid grid-cols-2 gap-2">
+					<a
+						class="small-link"
+						href={`/whatsapp?especialista=${encodeURI(
+							"Oftalmologia"
+						)}`}
+					>
+						Oftalmologia
+					</a>
+					<a
+						class="small-link"
+						href={`/whatsapp?especialista=${encodeURI(
+							"Ginecologia"
+						)}`}
+					>
+						Ginecologia
+					</a>
+					<a
+						class="small-link"
+						href={`/whatsapp?especialista=${encodeURI(
+							"Pediatria"
+						)}`}
+					>
+						Pediatria
+					</a>
+					<a
+						class="small-link"
+						href={`/whatsapp?especialista=${encodeURI(
+							"Ultrassonografia"
+						)}`}
+					>
+						Ultrassonografia
+					</a>
+					<a
+						class="small-link"
+						href={`/whatsapp?especialista=${encodeURI(
+							"Clínica Geral"
+						)}`}
+					>
+						Clínica Geral
+					</a>
+					<a
+						class="small-link"
+						href={`/whatsapp?especialista=${encodeURI(
+							"Psicologia"
+						)}`}
+					>
+						Psicologia
+					</a>
+					<a
+						class="small-link"
+						href={`/whatsapp?especialista=${encodeURI("Nutrição")}`}
+					>
+						Nutrição
+					</a>
+					<a
+						class="small-link"
+						href={`/whatsapp?especialista=${encodeURI(
+							"Nutrição Infantil"
+						)}`}
+					>
+						Nutrição Infantil
+					</a>
+					<a
+						class="small-link"
+						href={`/whatsapp?especialista=${encodeURI(
+							"Neurologia"
+						)}`}
+					>
+						Neurologia
+					</a>
+					<a
+						class="small-link"
+						href={`/whatsapp?especialista=${encodeURI("Exames")}`}
+					>
+						Exames
+					</a>
+				</div>
+
 				<a href="https://instagram.com/smartmed_ceara" class="link">
-					<span class="hidden md:block">
-						Conheça nossas especialidades
-					</span>
+					<span class="hidden md:block">Conheça nossos médicos</span>
 					<span class=" md:hidden">Nossas especialidades</span>
 				</a>
 				<a
--- src/routes/whatsapp/index.tsx
+++ src/routes/whatsapp/index.tsx
@@ -2,11 +2,14 @@ import { type RequestHandler } from "@builder.io/qwik-city";
 
 export const onGet: RequestHandler = (request) => {
 	const source = request.url.searchParams.get("source");
+	const especialista = request.url.searchParams.get("especialista");
 
 	throw request.redirect(
 		302,
-		`https://wa.me/5588997925279?text=Gostaria de agendar uma consulta.${
-			source ? encodeURI(" Vim através da página " + source) : ""
-		}`
+		`https://wa.me/5588997925279?text=${
+			especialista
+				? "Gostaria de agendar uma Consulta de " + especialista + "."
+				: "Gostaria de agendar uma Consulta."
+		}${source ? encodeURI(" Vim através da página " + source) : ""}`
 	);
 };
