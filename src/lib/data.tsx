47e99d33dcb72308b61ff2427b20f012d40f44cd
Chico Sousa <ochicosousa@gmail.com>
2024-11-13 11:41:07 -0300

    ginecologia

--- src/lib/data.tsx
+++ src/lib/data.tsx
@@ -1,66 +1,67 @@
 export const especialidades = [
-	{
-		id: 1,
-		title: "Oftalmologia",
-		text: "Quero marcar uma consulta Oftalmológica.",
-	},
-	{
-		id: 2,
-		title: "Psi. Autista",
-		text: "Quero marcar um horário com o Psicólogo para uma criança autista.",
-	},
-	{
-		id: 3,
-		title: "Pediatria",
-		text: "Quero marcar uma consulta Pediátrica.",
-	},
-	
-	{
-		id: 5,
-		title: "Clínica Geral",
-		text: "Quero marcar uma consulta com um Clínico Geral.",
-	},
-	
-	{
-		id: 8,
-		title: "Dermatologia",
-		text: "Quero marcar uma consulta com o Dermatologista.",
-	},
-	{
-		id: 9,
-		title: "Consulta Cardio",
-		text: "Quero marcar uma consulta Cardiológica.",
-	},
-	
-	{
-		id: 12,
-		title: "Psiquiatria",
-		text: "Quero marcar uma consulta Neurológica.",
-	},
-	{
-		id: 4,
-		title: "Ultrassonografia",
-		text: "Quero marcar uma Ultrassonografia.",
-	},
-	{
-		id: 10,
-		title: "Ecocardiograma",
-		text: "Quero marcar um ECOCARDIOGRAMA.",
-	},
-	{
-		id: 11,
-		title: "Eletrocardiograma",
-		text: "Quero marcar um ELETROCARDIOGRAMA.",
-	},
-	{
-		id: 6,
-		title: "Psicologia",
-		text: "Quero marcar um horário com a Psicóloga.",
-	},
-	{
-		id: 7,
-		title: "Nutrição",
-		text: "Quero marcar uma consulta com a Nutricionista.",
-	},
-
-];
+  {
+    id: 1,
+    title: 'Oftalmologia',
+    text: 'Quero marcar uma consulta Oftalmológica.'
+  },
+  {
+    id: 2,
+    title: 'Psi. Autista',
+    text: 'Quero marcar um horário com o Psicólogo para uma criança autista.'
+  },
+  {
+    id: 3,
+    title: 'Pediatria',
+    text: 'Quero marcar uma consulta Pediátrica.'
+  },
+  {
+    id: 4,
+    title: 'Ultrassonografia',
+    text: 'Quero marcar uma Ultrassonografia.'
+  },
+  {
+    id: 5,
+    title: 'Clínica Geral',
+    text: 'Quero marcar uma consulta com um Clínico Geral.'
+  },
+  {
+    id: 6,
+    title: 'Psicologia',
+    text: 'Quero marcar um horário com a Psicóloga.'
+  },
+  {
+    id: 7,
+    title: 'Nutrição',
+    text: 'Quero marcar uma consulta com a Nutricionista.'
+  },
+  {
+    id: 8,
+    title: 'Dermatologia',
+    text: 'Quero marcar uma consulta com o Dermatologista.'
+  },
+  {
+    id: 9,
+    title: 'Consulta Cardio',
+    text: 'Quero marcar uma consulta Cardiológica.'
+  },
+  {
+    id: 10,
+    title: 'Ecocardiograma',
+    text: 'Quero marcar um ECOCARDIOGRAMA.'
+  },
+  {
+    id: 11,
+    title: 'Eletrocardiograma',
+    text: 'Quero marcar um ELETROCARDIOGRAMA.'
+  },
+  {
+    id: 12,
+    title: 'Psiquiatria',
+    text: 'Quero marcar uma consulta Neurológica.'
+  },
+  {
+    id: 13,
+    title: 'Ginecologia',
+    text: 'Quero marcar uma consulta com a Ginecologista.'
+  }
+]
